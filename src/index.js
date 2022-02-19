2d97b413a67d2fc696ed7ba54a3c8842145d7714
UiguunaMikhailova <uyguuna1997@yandex.ru>
2022-02-20 01:39:35 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    n = String(n)
+    let str = ''
+    for (let i = 0; i < n.length; i++) {
+      str = `${n[i]}${str}`
+    }
+    return str
+  }
