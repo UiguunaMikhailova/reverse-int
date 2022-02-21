461e2989f69e8c28a079d4b136dfb2be8e3d070c
UiguunaMikhailova <uyguuna1997@yandex.ru>
2022-02-21 20:49:53 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function reverse (n) {
-    n = String(n)
+    let numStr = String(Math.abs(n))
     let str = ''
-    for (let i = 0; i < n.length; i++) {
-      str = `${n[i]}${str}`
+    for (let i = 0; i < numStr.length; i++) {
+      str = `${numStr[i]}${str}`
     }
     return str
   }
